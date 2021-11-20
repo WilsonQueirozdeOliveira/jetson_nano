e1617d738245355acc9ec26333b201b137b69cf8
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2021-11-19 22:00:02 -0300

    add tensor example

---
+++ pytorch/tensor.py
@@ -0,0 +1,16 @@
+import torch
+
+x = torch.Tensor([5,3])
+y = torch.Tensor([2,1])
+
+print("x = ",x ,"\n")
+
+print("y = ",y ,"\n")
+
+print("x*y = ",x*y,"\n")
+
+z = torch.zeros([2,5])
+
+print("z = ",z,"\n")
+
+print("z shape = ",z.shape,"\n") 
