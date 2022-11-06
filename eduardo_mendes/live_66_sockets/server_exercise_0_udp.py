e689f7156b818d46f2cb2a442cc2e6cea543a2ed
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-11-06 13:12:14 -0300

    added server_exercise_0_udp.py file

--- eduardo_mendes/live_66_sockets/server_exercise_0_udp.py
+++ eduardo_mendes/live_66_sockets/server_exercise_0_udp.py
@@ -3,14 +3,23 @@ import socket
 
 HOST = '192.168.3.200'                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
+
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 
-                        socket.IPPROTO_UDPLITE) as s:
+                        socket.IPPROTO_UDP) as s:
     s.bind((HOST, PORT))
-    s.listen(1)
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data: break
-            conn.sendall(data)
+    
+    print("UDP server up and listening")
+
+    data_recived = 0
+
+    while(not data_recived):
+
+        data_recived = s.recvfrom(1024)
+        
+        print('Received data_recived[0]: ', data_recived[0])# client massage
+        
+        print('Received data_recived[1]: ', data_recived[1])# client adress
+  
+        # Sending a reply to client
+        s.sendto(b'hello from server udp', data_recived[1])
+        
