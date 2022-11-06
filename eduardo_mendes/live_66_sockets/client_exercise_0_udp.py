46907ad8770b4cf12e83de4face485d9c77044d9
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-11-06 17:11:09 +0100

    update client_exercise_0_udp.py

---
+++ eduardo_mendes/live_66_sockets/client_exercise_0_udp.py
@@ -0,0 +1,18 @@
+# Echo client program
+import socket
+
+HOST = '192.168.3.200'    # The remote host
+PORT = 50007              # The same port as used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 
+                        socket.IPPROTO_UDP) as s:
+    
+    s.sendto(b'Hello, world from client udp',(HOST, PORT))
+
+    data_recived = s.recvfrom(1024)
+
+    print('Received: ', repr(data_recived))
+
+    print('Received data[0]: ', data_recived[0])
+
+    print('Received data[1]: ', data_recived[1])
