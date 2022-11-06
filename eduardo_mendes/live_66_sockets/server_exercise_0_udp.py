d7462ecb2d32fd31f6428bacd8e3522d682d1cbe
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-11-06 12:09:04 -0300

    changed to udp protocol

--- eduardo_mendes/live_66_sockets/server_exercise_0_udp.py
+++ eduardo_mendes/live_66_sockets/server_exercise_0_udp.py
@@ -3,7 +3,8 @@ import socket
 
 HOST = '192.168.3.200'                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 
+                        socket.IPPROTO_UDPLITE) as s:
     s.bind((HOST, PORT))
     s.listen(1)
     conn, addr = s.accept()
