23658f5c5396e8adfd5661c18ad4f6292fa9ff78
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-11-10 23:10:13 -0300

    solving bugs

--- python_code/socket/socket_byte/server_byte.py
+++ python_code/socket/socket_byte/server_byte.py
@@ -7,12 +7,14 @@ BUFER_SIZE = 1024
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as s:
 
-    s.sendto(b'string from server',(HOST, PORT))
+    s.bind((HOST, PORT))
 
     data_from_client = 0
 
     while(not data_from_client):
 
-        data_from_client = s.recvfrom(1024)
+        data_from_client = s.recvfrom(BUFER_SIZE)
 
         print('data_from_client', data_from_client)
+
+        s.sendto(b'string from server',(HOST, PORT))
