28dae1e4a09db118f9daa2d3f5cc6940969d75ee
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-11-13 19:30:02 -0300

    added pca9685_jetson.py file to control pwm driver model pca9685

---
+++ python_code/pca9685/pca9685_jetson.py
@@ -0,0 +1,13 @@
+from pca9685_driver import Device
+
+# 0x40 from i2cdetect -y 1 (1 if Raspberry pi 2)
+dev = Device(0x40,1)
+
+# set the duty cycle for LED05 to 50%
+dev.set_pwm(5, 4094)# max 4094 not (2**12=4096)
+
+# set the pwm frequency (Hz)
+dev.set_pwm_frequency(50)
+
+# servo channel 0
+dev.set_pwm(0, 300)
