af573fff051215767b7bc1a0d0194aa3db7c57dd
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-11-14 17:06:47 -0300

    added new library based on pid.py with code fixes, and math errors fixes

---
+++ python_code/pid/pid_lib.py
@@ -0,0 +1,40 @@
+from time import sleep
+
+class pid:
+    def __init__(self, output, setpoint,
+        feedback, Kp, Ki, Kd, sample_time):
+        self.output = output
+        self.setpoint = setpoint
+        self.feedback = feedback
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.sample_time = sample_time
+        self.last_erro = 0
+
+    def pid_setpoint_(self,feedback,setpoint,sample_time):
+
+        self.feedback = feedback # new feedback
+        self.setpoint = setpoint # new setpoint
+        self.sample_time = sample_time #new semple_time
+
+        sleep(self.sample_time)
+
+        erro = self.setpoint - self.feedback
+        # KP
+        KP = erro*self.Kp
+        # KI
+        KI = erro*(self.Ki*self.sample_time)
+        # KD
+        KD = self.Kd*((self.last_erro-erro)/self.sample_time)
+        # camp KD to 0
+        if (KD > 1000) or (KD < -1000) :
+            KD = 0
+        self.last_erro = erro
+        #print('KD: ',KD)
+        
+        # output
+        self.output = (feedback + KP + KI + KD)
+        #print(self.output)
+
+        return self.output
