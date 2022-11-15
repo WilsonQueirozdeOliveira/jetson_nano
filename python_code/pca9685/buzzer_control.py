e1fa2d8330eaba84e67d5c3b1e4d8f42c423b17e
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-11-15 18:32:35 -0300

    updated comments

--- python_code/pca9685/buzzer_control.py
+++ python_code/pca9685/buzzer_control.py
@@ -3,10 +3,9 @@ from pca9685_driver import Device
 
 dev = Device(0x40,1)
 # set the pwm frequency (Hz)
-dev.set_pwm_frequency(910)
+dev.set_pwm_frequency(910)# 920 max volume
 while True:
-    # set the duty cycle for LED05 to 50% = 2047
-    dev.set_pwm(5, 2000)# max 4094 not (2**12=4096)
+    dev.set_pwm(5, 2000)# 2000 max volume
     time.sleep(1)
     dev.set_pwm(5, 0)
     time.sleep(1)
