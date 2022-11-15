cc9ddb589d67f59c3d4fe7ca99da8360d439bf4b
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-11-15 15:11:40 -0300

    update comments

--- python_code/pca9685/pca9685_jetson.py
+++ python_code/pca9685/pca9685_jetson.py
@@ -3,7 +3,7 @@ from pca9685_driver import Device
 # 0x40 from i2cdetect -y 1 (1 if Raspberry pi 2)
 dev = Device(0x40,1)
 
-# set the duty cycle for LED05 to 50%
+# set the duty cycle for LED05 to 50% = 2047
 dev.set_pwm(5, 4094)# max 4094 not (2**12=4096)
 
 # set the pwm frequency (Hz)
