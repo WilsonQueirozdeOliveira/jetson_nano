e5f6f4be3597ca29c67dca5f24c75b145e5c9771
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2022-12-04 20:51:28 -0300

    update reverse control

--- python_code/wheel_pid/wheel_control_pid.py
+++ python_code/wheel_pid/wheel_control_pid.py
@@ -12,23 +12,23 @@ wheel_output_pwm = 1
 
 whell_rear_left = wheel_sensor(gpio_input=15,tire_diameter_m=0.067)
 
-pid_whell = pid(output=0, setpoint=5,feedback=0, Kp=0.1, Ki=0.01, Kd=0)
+pid_whell = pid(output=0, setpoint=3,feedback=0, Kp=0.1, Ki=1, Kd=0)
 
 pid_wheel_output_pwm = 300
 
 dev.set_pwm(wheel_output_pwm, pid_wheel_output_pwm)
 
 count = 0
-while count < 50:
+while count < 200:
     count += 1
     print('count: ', count)
     print('m/s: ', whell_rear_left.speed_meters_per_second())
 
     feedback = whell_rear_left.speed_meters_per_second() # sensor from output
-    pid_output = pid_whell.pid_update_(feedback=feedback,setpoint=5)
+    pid_output = pid_whell.pid_update_(feedback=feedback,setpoint=3)
     print('pid_output:',pid_output)
 
-    pid_wheel_output_pwm = int(330-(pid_output*3))
+    pid_wheel_output_pwm = int(330-(pid_output)+0.2)
 
     print('pid_wheel_output_pwm: ',pid_wheel_output_pwm)
 
