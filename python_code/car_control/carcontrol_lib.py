950bbc3c8e3a2ce9da951f031c2bca9b3e6c8098
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2023-03-12 17:17:28 -0300

    code in the process of being updated, to use a library written in c to read the vehicle's speed

--- python_code/car_control/carcontrol_lib.py
+++ python_code/car_control/carcontrol_lib.py
@@ -4,6 +4,7 @@ sys.path.insert(1, '../actuators')
 sys.path.insert(1, '../pid')
 
 from odometer_lib import odometer
+from odometer_lib import c_odometer
 from actuators_lib import Actuators
 from pid_lib import pid
 import time
@@ -19,13 +20,15 @@ class CarControl:
         self.odometer = odometer(wheel_sensor_pin_rear_left, wheel_sensor_pin_rear_right, wheel_diameter_m)  # initialize the odometer
         self.speed_pid = pid(0, 0, 0, 0.4, 8, 0)  # initialize the speed PID controller
         self.direction = 0
-        self.odometer_thread = threading.Thread(target=self._update_odometer_loop, daemon=True)
-        self.odometer_thread.start()
+        #self.odometer_thread = threading.Thread(target=self._update_odometer_loop, daemon=True)
+        #self.odometer_thread.start()
 
-    def _update_odometer_loop(self):
-        while True:
-            self.odometer.update()
-            time.sleep(0.1)  # adjust update rate as needed
+        self.c_odometer = c_odometer()
+
+    #def _update_odometer_loop(self):
+    #    while True:
+    #        self.odometer.update()
+    #        time.sleep(0.1)  # adjust update rate as needed
 
     def set_steer(self, setpoint):
         feedback = self.output_steer
@@ -33,7 +36,8 @@ class CarControl:
         self.actuators.set_steer(self.output_steer)  # set steering angle
 
     def set_speed(self, setpoint):
-        feedback = self.odometer.speed_avg  # get current speed for feedback term
+        #feedback = self.odometer.speed_avg  # get current speed for feedback term
+        feedback = self.c_odometer.update_c_odometer()  # get current avg_speed from c lib
         print('self.odometer.speed_avg: ', self.odometer.speed_avg)
         self.output_speed = self.speed_pid.pid_update_(feedback, setpoint)  # update PID controller
         print('self.output_speed: ', self.output_speed)
