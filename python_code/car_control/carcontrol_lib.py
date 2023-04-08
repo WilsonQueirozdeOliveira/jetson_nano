b432ade0b22e0b84599b612704fc3f6b2fddfa0b
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2023-04-08 18:25:17 -0300

    update to use new sensor lib

--- python_code/car_control/carcontrol_lib.py
+++ python_code/car_control/carcontrol_lib.py
@@ -1,9 +1,8 @@
 import sys
-sys.path.insert(1, '../odometer')
-sys.path.insert(1, '../actuators')
-sys.path.insert(1, '../pid')
+sys.path.append("/home/jetson/jetson_nano/python_code/odometer")
+sys.path.append("/home/jetson/jetson_nano/python_code/actuators")
+sys.path.append("/home/jetson/jetson_nano/python_code/pid")
 
-from odometer_lib import odometer
 from odometer_lib import c_odometer
 from actuators_lib import Actuators
 from pid_lib import pid
@@ -17,18 +16,19 @@ class CarControl:
         self.actuators = Actuators(steering_channel, motor_channel)  # initialize the actuators
         self.output_steer = 0
         self.output_speed = 0
-        self.odometer = odometer(wheel_sensor_pin_rear_left, wheel_sensor_pin_rear_right, wheel_diameter_m)  # initialize the odometer
-        self.speed_pid = pid(0, 0, 0, 0.4, 8, 0)  # initialize the speed PID controller
+        self.odometer = c_odometer()  # initialize the odometer
+        self.speed_pid = pid(0, 0, 0, 50.0, 400.0, 0.0)  # initialize the speed PID controller
         self.direction = 0
-        #self.odometer_thread = threading.Thread(target=self._update_odometer_loop, daemon=True)
-        #self.odometer_thread.start()
+        self.speed_feedback = 0
+        #self.speed_feedback = self.odometer.update_c_odometer()
+        self.odometer_thread = threading.Thread(target=self._update_odometer_loop, daemon=True)
+        self.odometer_thread.start()
 
-        self.c_odometer = c_odometer()
-
-    #def _update_odometer_loop(self):
-    #    while True:
-    #        self.odometer.update()
-    #        time.sleep(0.1)  # adjust update rate as needed
+    def _update_odometer_loop(self):
+        while True:
+            self.speed_feedback = self.odometer.update_c_odometer()
+            print(" ***loop self.speed_feedback: \n",self.speed_feedback)
+            #time.sleep(0.1)  # adjust update rate as needed
 
     def set_steer(self, setpoint):
         feedback = self.output_steer
@@ -36,14 +36,14 @@ class CarControl:
         self.actuators.set_steer(self.output_steer)  # set steering angle
 
     def set_speed(self, setpoint):
-        #feedback = self.odometer.speed_avg  # get current speed for feedback term
-        feedback = self.c_odometer.update_c_odometer()  # get current avg_speed from c lib
-        print('self.odometer.speed_avg: ', self.odometer.speed_avg)
+        #self.speed_feedback = self.odometer.update_c_odometer()
+        feedback = self.speed_feedback
+        print('self.odometer.speed_avg: ', feedback)# get current avg_speed from c lib
         self.output_speed = self.speed_pid.pid_update_(feedback, setpoint)  # update PID controller
         print('self.output_speed: ', self.output_speed)
         
         if self.direction == "forward":
-            pid_wheel_output_pwm = 70*(self.output_speed)
+            pid_wheel_output_pwm = (self.output_speed)
             self.actuators.set_motor_forward(pid_wheel_output_pwm)  # set motor speed forward
         elif self.direction == "reverse":
             pid_wheel_output_pwm = 70*(self.output_speed)
