03d945915744fa59e5e6dadacf11b8aab8d2db06
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2023-04-08 18:29:36 -0300

    update to use with new sensor lib

--- python_code/odometer/odometer_lib_test.py
+++ python_code/odometer/odometer_lib_test.py
@@ -1,32 +1,9 @@
-#from odometer_lib import odometer
-#import time
-
+# Import the c_odometer class
 from odometer_lib import c_odometer
 
-c_odometer = c_odometer()
+# Create an instance of the c_odometer class
+odometer = c_odometer()
 
-while (true)
-    avg_speed = c_odometer.avg_speed()
-
-
-'''
-# GPIO input pins for the left and right wheels
-GPIO_INPUT_LEFT = 15
-GPIO_INPUT_RIGHT = 29
-
-# Diameter of the tire in meters
-TIRE_DIAMETER_M = 0.067
-
-# Create an instance of the odometer class
-my_odometer = odometer(GPIO_INPUT_LEFT, GPIO_INPUT_RIGHT, TIRE_DIAMETER_M)
-
-# Update the odometer readings periodically
-while True:
-    my_odometer.update()
-    print("Left distance:", my_odometer.distance_left, "meters")
-    print("Right distance:", my_odometer.distance_right, "meters")
-    print("Total distance:", my_odometer.distance_total, "meters")
-    print("Average speed:", my_odometer.speed_avg, "meters per second")
-    time.sleep(1)
-
-'''
+# Update the average speed using the c_odometer instance and print it
+avg_speed = odometer.update_c_odometer()
+print("Average speed:", avg_speed)
