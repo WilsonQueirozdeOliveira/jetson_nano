da5e82d0e93e7b87dfeaf3877f4beaccf77873c7
WilsonQueirozdeOliveira <wilson.queiroz01@gmail.com>
2023-04-08 18:27:18 -0300

    update to use with new sensor lib

--- python_code/car_control/carcontrol_lib_test.py
+++ python_code/car_control/carcontrol_lib_test.py
@@ -1,9 +1,17 @@
+import sys
+sys.path.append("/path/to/pca9685_driver")
 from carcontrol_lib import CarControl
 import time
 
 car = CarControl(0, 1, 15, 29, 6.7)  # initialize with steering channel 0, motor channel 1, wheel sensor pin 2, and wheel radius 5 cm
 
 car.set_stop()
+time.sleep(1)
+car.set_steer(0)
+time.sleep(1)
+car.set_steer(100)
+time.sleep(1)
+car.set_steer(50)
 time.sleep(1)
 
 count = 0
@@ -12,9 +20,9 @@ while count < 200:
     car.set_steer(50)  # set the steering angle to 50 (0 to 100)
 
     car.set_direction("forward")  # start moving forward
-    car.set_speed(0.9)  # set the car speed to x m/s
+    car.set_speed(1.5)  # set the car speed to x m/s
     
-    time.sleep(0.1)  # wait for 1 seconds
+    #time.sleep(0.001)  # wait for 1 seconds
 
 car.set_stop()
 car.set_stop()
@@ -22,6 +30,6 @@ time.sleep(1)
 car.set_stop()
 time.sleep(1)
 
-distance = car.get_distance()  # get the distance traveled during the last 5 seconds
-print('distance: ', distance)
+#distance = car.get_distance()  # get the distance traveled during the last 5 seconds
+#print('distance: ', distance)
 #car.cleanup()  # clean up the actuators and odometer
